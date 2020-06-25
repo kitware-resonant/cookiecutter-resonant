6456866e37dc96385ff352d03189ab8ddca8d2e5
Brian Helba <brian.helba@kitware.com>
2020-06-25 13:36:04 -0400

    Uppercase Bash variables

--- dev/source-native-env.sh
+++ dev/source-native-env.sh
@@ -1,5 +1,5 @@
 # This file must be sourced, not run
-dotenv_file="$( dirname "${BASH_SOURCE[0]}" )/.env.docker-compose-native"
-while read -r var; do
-  export "${var?}"
-done < "${dotenv_file}"
+DOTENV_FILE="$( dirname "${BASH_SOURCE[0]}" )/.env.docker-compose-native"
+while read -r VAR; do
+  export "${VAR?}"
+done < "${DOTENV_FILE}"
