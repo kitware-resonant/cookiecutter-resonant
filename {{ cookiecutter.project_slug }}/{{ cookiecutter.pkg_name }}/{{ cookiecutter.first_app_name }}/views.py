ff8f3b30b67b2d054e896614368f118f2f5a2b44
Dan LaManna <dan.lamanna@kitware.com>
2020-06-29 15:57:00 -0400

    Add default views

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.pkg_name }}/{{ cookiecutter.first_app_name }}/views.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.pkg_name }}/{{ cookiecutter.first_app_name }}/views.py
@@ -1,3 +1,26 @@
-# from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.db.models import Count, Q
+from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+
+from .models import Image
+
+
+class GalleryView(ListView):
+    queryset = Image.objects.order_by('created')
+    template_name = 'gallery.html'
+    paginate_by = 20
+
+
+def image_summary(request):
+    return render(
+        request,
+        'summary.html',
+        {
+            'user_summary': User.objects.annotate(
+                processed_images=Count('image', filter=Q(image__checksum__isnull=False)),
+                unprocessed_images=Count('image', filter=Q(image__checksum__isnull=True)),
+            )
+        },
+    )
