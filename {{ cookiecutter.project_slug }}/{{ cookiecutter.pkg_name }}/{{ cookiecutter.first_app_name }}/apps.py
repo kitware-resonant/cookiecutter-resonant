2ac1dbfff675d5f9c46e28f7ef35724f6d49b19d
Brian Helba <brian.helba@kitware.com>
2020-07-06 17:40:07 -0400

    Support multi-component first_app_name

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.pkg_name }}/{{ cookiecutter.first_app_name }}/apps.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.pkg_name }}/{{ cookiecutter.first_app_name }}/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class {{ cookiecutter.first_app_name.capitalize() }}Config(AppConfig):
+class {{ cookiecutter.first_app_name.split('_')|map('capitalize')|join('') }}Config(AppConfig):
     name = '{{ cookiecutter.pkg_name }}.{{ cookiecutter.first_app_name }}'
-    verbose_name = '{{ cookiecutter.project_name }}: {{ cookiecutter.first_app_name.capitalize() }}'
+    verbose_name = '{{ cookiecutter.project_name }}: {{ cookiecutter.first_app_name.split('_')|map('capitalize')|join(' ') }}'
