c75677ac52a2cb31a69d9d7e304c6c888ce67f30
Brian Helba <brian.helba@kitware.com>
2020-07-20 11:23:29 -0400

    Simplify Image.compute_checksum

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.pkg_name }}/{{ cookiecutter.first_app_name }}/tasks.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.pkg_name }}/{{ cookiecutter.first_app_name }}/tasks.py
@@ -6,5 +6,5 @@ from {{ cookiecutter.pkg_name }}.{{ cookiecutter.first_app_name }}.models import
 @shared_task()
 def image_compute_checksum(image_id: int):
     image = Image.objects.get(pk=image_id)
-    image.checksum = image.compute_checksum()
+    image.compute_checksum()
     image.save()
