49f5fca422a220ea6093c9247cc2f28f81844780
Zach Mullen <zach.mullen@kitware.com>
2020-09-10 10:48:16 -0400

    Remove trailing slash from REST API

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.pkg_name }}/{{ cookiecutter.first_app_name }}/tests/test_image.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.pkg_name }}/{{ cookiecutter.first_app_name }}/tests/test_image.py
@@ -10,7 +10,7 @@ def test_image_checksum(image_factory):
 
 @pytest.mark.django_db
 def test_image_rest_retrieve(api_client, image):
-    resp = api_client.get(f'/api/v1/images/{image.id}/')
+    resp = api_client.get(f'/api/v1/images/{image.id}')
     assert resp.status_code == 200
     # Inspect .data to avoid parsing the response content
     assert resp.data['name'] == image.name
