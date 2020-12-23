815e79372c2622a492b4779fdc1788890a8271a1
Dan LaManna <dan.lamanna@kitware.com>
2020-12-23 11:13:52 -0500

    Add branding to django admin

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.pkg_name }}/{{ cookiecutter.first_app_name }}/admin/__init__.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.pkg_name }}/{{ cookiecutter.first_app_name }}/admin/__init__.py
@@ -1,3 +1,9 @@
+from django.contrib import admin
+
 from .image import ImageAdmin
 
 __all__ = ['ImageAdmin']
+
+admin.site.site_header = '{{ cookiecutter.project_name }}'
+admin.site.site_title = '{{ cookiecutter.project_name }}'
+admin.site.index_title = ''
