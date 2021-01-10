0c60e45e4bd5e9324c638a7421461f546ec118d8
Christopher Kotfila <chris.kotfila@kitware.com>
2021-01-10 17:51:29 -0500

    Make specific to ZSH but accessible from any pwd

--- {{ cookiecutter.project_slug }}/dev/source-native-env.sh
+++ {{ cookiecutter.project_slug }}/dev/source-native-env.sh
@@ -1,13 +1,12 @@
 # This file must be sourced, not run
 DOTENV_FILE="$( dirname "${BASH_SOURCE[0]}" )/.env.docker-compose-native"
 
-# This is a hack to make sourcing source-native-env.sh compliant with shells
-# like ZSH See: https://github.com/girder/cookiecutter-django-girder/issues/1
+# This is a hack to make sourcing source-native-env.sh compliant with ZSH
+# See: https://github.com/girder/cookiecutter-django-girder/issues/1
 if [[ ! -r $DOTENV_FILE ]]; then
-  if [[ -r dev/.env.docker-compose-native ]]; then
-    DOTENV_FILE=dev/.env.docker-compose-native
-  else
-    echo >&2 "Could not find .env.docker-compose-native, are you sourcing from the root directory?"
+  DOTENV_FILE=$( dirname $0:A )/.env.docker-compose-native
+  if [[ ! -r $DOTENV_FILE ]]; then
+    echo >&2 "Could not find .env.docker-compose-native, try sourcing $0 from the dev/ directory."
     return
   fi
 fi
