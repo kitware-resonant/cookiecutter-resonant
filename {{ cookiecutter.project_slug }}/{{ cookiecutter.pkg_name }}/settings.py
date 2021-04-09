ee5fec341333b3736300f7587e9f3d0e5725867f
Brian Helba <brian.helba@kitware.com>
2021-04-09 00:49:53 -0400

    Upgrade django-composed-configuration

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.pkg_name }}/settings.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.pkg_name }}/settings.py
@@ -19,7 +19,7 @@ class {{ cookiecutter.pkg_name.split('_')|map('capitalize')|join('') }}Mixin(Con
     BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
     @staticmethod
-    def before_binding(configuration: ComposedConfiguration) -> None:
+    def mutate_configuration(configuration: ComposedConfiguration) -> None:
         # Install local apps first, to ensure any overridden resources are found first
         configuration.INSTALLED_APPS = [
             '{{ cookiecutter.pkg_name }}.{{ cookiecutter.first_app_name }}.apps.{{ cookiecutter.first_app_name.split('_')|map('capitalize')|join('') }}Config',
