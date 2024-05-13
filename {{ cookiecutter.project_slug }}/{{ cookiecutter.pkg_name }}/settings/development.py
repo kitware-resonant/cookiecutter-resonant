c94dabdec9a06b9702920fd66cbec200d8894257
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2024-05-13 19:45:55 -0400

    Add `django-browser-reload` to debug tooling

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.pkg_name }}/settings/development.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.pkg_name }}/settings/development.py
@@ -6,6 +6,7 @@ from resonant_settings.development.extensions import *  # isort: skip
 
 INSTALLED_APPS += [
     'debug_toolbar',
+    'django_browser_reload',
     'django_extensions',
 ]
 # Force WhiteNoice to serve static files, even when using 'manage.py runserver'
@@ -16,6 +17,9 @@ INSTALLED_APPS.insert(staticfiles_index, 'whitenoise.runserver_nostatic')
 # However, it must come after any other middleware that encodes the response’s content,
 # such as GZipMiddleware.
 MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
+MIDDLEWARE += [
+    'django_browser_reload.middleware.BrowserReloadMiddleware',
+]
 
 # DEBUG is not enabled for testing, to maintain parity with production.
 # Also, do not directly reference DEBUG when toggling application features; it's more sustainable
