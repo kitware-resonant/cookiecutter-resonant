67957459589da99522f5c473d5003b17141674e6
Brian Helba <brian.helba@kitware.com>
2025-01-24 00:23:49 -0500

    Use a unified environment variable to set the MinIO URL

--- django-resonant-settings/resonant_settings/testing/minio_storage.py
+++ django-resonant-settings/resonant_settings/testing/minio_storage.py
@@ -1,26 +1,23 @@
 """
 Configure MinioMediaStorage.
 
-The following environment variables must be externally set:
-* DJANGO_MINIO_STORAGE_ENDPOINT
-* DJANGO_MINIO_STORAGE_ACCESS_KEY
-* DJANGO_MINIO_STORAGE_SECRET_KEY
-* DJANGO_STORAGE_BUCKET_NAME
-
 This requires the `django-minio-storage` package to be installed.
 """
+from urllib.parse import ParseResult
 
 from resonant_settings._env import env
 
-MINIO_STORAGE_ENDPOINT: str = env.str("DJANGO_MINIO_STORAGE_ENDPOINT")
-MINIO_STORAGE_USE_HTTPS: bool = env.bool("DJANGO_MINIO_STORAGE_USE_HTTPS", default=False)
-MINIO_STORAGE_ACCESS_KEY: str = env.str("DJANGO_MINIO_STORAGE_ACCESS_KEY")
-MINIO_STORAGE_SECRET_KEY: str = env.str("DJANGO_MINIO_STORAGE_SECRET_KEY")
-# Use this name for unity with the S3 configuration
-MINIO_STORAGE_MEDIA_BUCKET_NAME: str = env.str("DJANGO_STORAGE_BUCKET_NAME")
+minio_url: ParseResult = env.url("DJANGO_MINIO_STORAGE_URL")
+MINIO_STORAGE_USE_HTTPS = minio_url.scheme == "https"
+MINIO_STORAGE_ENDPOINT = minio_url.hostname + (f":{minio_url.port}" if minio_url.port else "")
+MINIO_STORAGE_ACCESS_KEY = minio_url.username
+MINIO_STORAGE_SECRET_KEY = minio_url.password
+MINIO_STORAGE_MEDIA_BUCKET_NAME = minio_url.path.lstrip("/")
+
 # Setting this allows MinIO to work through network namespace partitions
 # (e.g. when running within Docker Compose)
 MINIO_STORAGE_MEDIA_URL: str | None = env.str("DJANGO_MINIO_STORAGE_MEDIA_URL", default=None)
+
 MINIO_STORAGE_AUTO_CREATE_MEDIA_BUCKET = True
 MINIO_STORAGE_AUTO_CREATE_MEDIA_POLICY = "READ_WRITE"
 MINIO_STORAGE_MEDIA_USE_PRESIGNED = True
