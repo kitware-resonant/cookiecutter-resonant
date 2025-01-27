a766bfb492eec9639cc048efd13dad68c699ecb6
Brian Helba <brian.helba@kitware.com>
2025-01-26 23:41:15 -0500

    Don't use 64-bit primary keys

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.pkg_name }}/settings/base.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.pkg_name }}/settings/base.py
@@ -73,7 +73,6 @@ DATABASES = {
         'CONN_MAX_AGE': timedelta(minutes=10).total_seconds(),
     }
 }
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STORAGES = {
     # Inject the default storage in particular run configurations
