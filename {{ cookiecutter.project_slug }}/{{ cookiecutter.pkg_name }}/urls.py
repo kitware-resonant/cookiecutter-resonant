62e3c71ed69789dd9dc0022538629c944b7a6273
Brian Helba <brian.helba@kitware.com>
2025-01-26 23:56:42 -0500

    Replace DRF installation with Django Ninja

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.pkg_name }}/urls.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.pkg_name }}/urls.py
@@ -1,37 +1,21 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions, routers
+from ninja import NinjaAPI
 
 {% if cookiecutter.include_example_code == 'yes' -%}
-from {{ cookiecutter.pkg_name }}.{{ cookiecutter.first_app_name }}.rest import ImageViewSet
 from {{ cookiecutter.pkg_name }}.{{ cookiecutter.first_app_name }}.views import GalleryView, image_summary
 
 {% endif -%}
 
-router = routers.SimpleRouter()
-{% if cookiecutter.include_example_code == 'yes' -%}
-router.register(r'images', ImageViewSet)
-
-{% endif -%}
-
-# OpenAPI generation
-schema_view = get_schema_view(
-    openapi.Info(title='{{ cookiecutter.project_name }}', default_version='v1', description=''),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
+api = NinjaAPI()
 
 urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('oauth/', include('oauth2_provider.urls')),
     path('admin/', admin.site.urls),
     path('api/v1/s3-upload/', include('s3_file_field.urls')),
-    path('api/v1/', include(router.urls)),
-    path('api/docs/redoc/', schema_view.with_ui('redoc'), name='docs-redoc'),
-    path('api/docs/swagger/', schema_view.with_ui('swagger'), name='docs-swagger'),
+    path('api/v1/', api.urls),
 {%- if cookiecutter.include_example_code == 'yes' %}
     path('summary/', image_summary, name='image-summary'),
     path('gallery/', GalleryView.as_view(), name='gallery'),
