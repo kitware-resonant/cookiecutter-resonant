60f451deb0bb9efd5a607dad4ea18bf2303fec64
Zach Mullen <zach.mullen@kitware.com>
2025-06-03 17:17:28 -0400

    Expose settings for eager celery tasks

--- django-resonant-settings/resonant_settings/development/celery.py
+++ django-resonant-settings/resonant_settings/development/celery.py
@@ -1,7 +1,13 @@
+from resonant_settings._env import env
+
 # Acknowledge early in development, which will help prevent failing or
 # long-running tasks from being started automatically every time the worker
 # process restarts; this more aggressively flushes the task queue.
 CELERY_TASK_ACKS_LATE = False
+
+# Expose task_always_eager configurability via the environment for debugging purposes.
+CELERY_TASK_ALWAYS_EAGER = env.bool("DJANGO_CELERY_TASK_ALWAYS_EAGER", default=False)
+CELERY_TASK_EAGER_PROPAGATES = CELERY_TASK_ALWAYS_EAGER
 
 # In development, run without concurrency.
 CELERY_WORKER_CONCURRENCY: int | None = 1
