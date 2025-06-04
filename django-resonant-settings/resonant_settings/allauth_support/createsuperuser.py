4f0026c10d50d80860f3441fa254fb66cbc209cb
Brian Helba <brian.helba@kitware.com>
2025-06-03 21:50:28 -0400

    Add additional MyPy options

--- django-resonant-settings/resonant_settings/allauth_support/createsuperuser.py
+++ django-resonant-settings/resonant_settings/allauth_support/createsuperuser.py
@@ -14,7 +14,9 @@ class Command(createsuperuser.Command):
         self.UserModel = EmailAsUsernameProxyUser
         self.username_field = self.UserModel._meta.get_field(self.UserModel.USERNAME_FIELD)
 
-    def _validate_username(self, username, verbose_field_name, database):
+    def _validate_username(
+        self, username: str, verbose_field_name: str, database: str
+    ) -> str | None:
         # Since "username" is actually unique, "email" (i.e. "self.username_field") is logically
         # unique too. Explicitly setting the "_unique" attribute ensures that app-level duplicate
         # checking is done by "_validate_username", which produces better, earlier error messages.
@@ -23,7 +25,9 @@ class Command(createsuperuser.Command):
         with temporarily_change_attributes(self.username_field, _unique=True):
             # Normalize (as it would be done before saving) for better duplicate detection
             username = self.UserModel.normalize_username(username)
-            return super()._validate_username(username, verbose_field_name, database)
+            return super()._validate_username(  # type: ignore[misc]
+                username, verbose_field_name, database
+            )
 
 
 class EmailAsUsernameProxyUserManager(UserManager):
