31ffe04b26184307c6c577aa40e33ee4d8544025
Jacob Nesbitt <jjnesbitt2@gmail.com>
2025-08-07 13:03:16 -0400

    Add UPDATE_ON_FETCH to the debug toolbar settings

--- django-resonant-settings/resonant_settings/development/debug_toolbar.py
+++ django-resonant-settings/resonant_settings/development/debug_toolbar.py
@@ -10,4 +10,7 @@ DEBUG_TOOLBAR_CONFIG = {
     "RESULTS_CACHE_SIZE": 250,
     # If this setting is True, large sql queries can cause the page to render slowly
     "PRETTIFY_SQL": False,
+    # Updates the currently shown request to the most recent ajax request. This is useful
+    # for the swagger page, where all the requests that we care about are ajax requests.
+    "UPDATE_ON_FETCH": True,
 }
