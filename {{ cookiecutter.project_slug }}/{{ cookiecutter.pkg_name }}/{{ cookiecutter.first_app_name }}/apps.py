7728f585026e4ded9951442b310f1f4b6506b608
Brian Helba <brian.helba@kitware.com>
2025-10-15 09:53:57 -0400

    Switch to using Copier for generation

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.pkg_name }}/{{ cookiecutter.first_app_name }}/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class {{ cookiecutter.first_app_name.split('_')|map('capitalize')|join('') }}Config(AppConfig):
-    name = '{{ cookiecutter.pkg_name }}.{{ cookiecutter.first_app_name }}'
-    verbose_name = '{{ cookiecutter.project_name }}: {{ cookiecutter.first_app_name.split('_')|map('capitalize')|join(' ') }}'
