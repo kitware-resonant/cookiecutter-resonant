5ac952eee9316357e8754c0a7047178cec8c32a9
Brian Helba <brian.helba@kitware.com>
2025-11-24 16:28:16 -0500

    Ensure that UserManager and type checking works with swappable user models

--- django-resonant-settings/resonant_settings/allauth_support/createsuperuser.py
+++ django-resonant-settings/resonant_settings/allauth_support/createsuperuser.py
@@ -1,14 +1,17 @@
 from __future__ import annotations
 
-from typing import Any, ClassVar
+from typing import TYPE_CHECKING, Any, ClassVar
 
 from django.contrib.auth import get_user_model
 from django.contrib.auth.management.commands import createsuperuser
-from django.contrib.auth.models import UserManager
 
 from resonant_settings.allauth_support.utils import temporarily_change_attributes
 
-User = get_user_model()
+if TYPE_CHECKING:
+    from django.contrib.auth.models import User, UserManager
+else:
+    User = get_user_model()
+    UserManager = User._default_manager.__class__
 
 
 class Command(createsuperuser.Command):
@@ -50,7 +53,7 @@ class EmailAsUsernameProxyUserManager(UserManager):
         return user
 
 
-class EmailAsUsernameProxyUser(User):  # type: ignore
+class EmailAsUsernameProxyUser(User):
     # https://github.com/typeddjango/django-stubs/issues/2112
     class Meta(User.Meta):  # type: ignore[name-defined]
         proxy = True
