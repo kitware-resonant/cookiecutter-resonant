641cef838734c492340c6ebd6983048640711194
Brian Helba <brian.helba@kitware.com>
2025-12-02 23:48:34 -0500

    Fix the `*` import from `resonant_settings.production.email` to include all settings

--- django-resonant-settings/resonant_settings/production/email.py
+++ django-resonant-settings/resonant_settings/production/email.py
@@ -8,14 +8,19 @@ The following environment variables must be externally set:
 * `DJANGO_DEFAULT_FROM_EMAIL`, as the default From address for outgoing email.
 """
 
+from typing import Any
+
 from resonant_settings._env import env
 
-vars().update(env.email_url("DJANGO_EMAIL_URL"))
+email_config: dict[str, Any] = env.email_url("DJANGO_EMAIL_URL")
+globals().update(email_config)
 
 DEFAULT_FROM_EMAIL: str = env.str("DJANGO_DEFAULT_FROM_EMAIL")
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
 
+
 __all__ = [
+    *email_config.keys(),
     "DEFAULT_FROM_EMAIL",
     "SERVER_EMAIL",
 ]
